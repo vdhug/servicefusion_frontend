03d9efcd60fbd374fe4b1c0493d030705f6569fc
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2020-02-01 15:57:42 -0500

    Creating chart component

---
+++ src/components/chart/Chart.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="chart">
+    <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+  </div>
+</template>
+
+<script>
+import PieChart from "./PieChart.js";
+export default {
+  name: "Chart",
+  components: {
+    PieChart
+  },
+  data() {
+    return {
+      chartOptions: {
+        hoverBorderWidth: 10
+      },
+      chartData: {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 5,
+        labels: ["Green", "Red", "Blue"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            data: [1, 10, 5]
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style>
+#chart {
+  max-height: 300px;
+  max-width: 32%;
+}
+</style>
