7098f78846ad92917c8c6c309123c76dbf53eca5
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2020-02-03 16:59:37 -0500

    adding new person route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AddPerson from '../views/AddPerson.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/new',
+    name: 'new',
+    component: AddPerson
   }
 ]
 
